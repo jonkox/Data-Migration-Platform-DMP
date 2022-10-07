c25720d67d690f10717eacc8933cefd188b3c3d7
Andrea Li Hernandez <andrealh17@estudiantec.cr>
2022-10-07 15:59:55 -0600

    agregar

---
+++ pruebas/conectarES_test.py
@@ -0,0 +1,8 @@
+import elasticsearch
+
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(['http://localhost:9200/'], verify_certs=True)
+
+if not es.ping():
+    raise ValueError("Connection failed")
