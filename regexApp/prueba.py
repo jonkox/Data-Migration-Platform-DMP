1b642b04908c8fd5fd7502cbf9f2b83fe954c080
Johnny Diaz <jhonny4605@estudiantec.cr>
2022-10-11 19:37:18 -0600

    Pruebas simples con REGEX y MariaDB

--- regexApp/prueba.py
+++
@@ -1,9 +0,0 @@
-import time
-import os
-
-DATA=os.getenv('DATAFROMK8S')
-while True:
-    localtime = time.localtime()
-    result = time.strftime("%I:%M:%S %p", localtime)
-    print(DATA+" - " +result)
-    time.sleep(1)
