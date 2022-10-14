1df99197bb3412261d860c04d0a15cb466284e7c
Dean-SF <dsanabriaf@estudiantec.cr>
2022-10-13 23:15:16 -0600

    SQL processor funcional

--- sqlprocessor/image/app/app.py
+++ sqlprocessor/image/app/app.py
@@ -1,4 +1,6 @@
 from elasticsearch import Elasticsearch
+import re as Regex
+import elastic_transport
 import mariadb
 import json
 import pika
@@ -12,12 +14,81 @@ RABBITPASS = "XQnpNkb23Z5RqAly" #os.getenv("RABBITPASS")
 RABBITCONSUMEQUEUE = "mysqlprocessor" #os.getenv("RABBITQUEUENAME")
 RABBITPUBLISHQUEUE = "sqlprocessor" #os.getenv("RABBITQUEUENAME")
 
+# Elastic
+ELASTICHOST = "http://localhost"#os.getenv("ELASTICHOST")
+ELASTICPORT = "32500"#os.getenv("ELASTICPORT")
+ELASTICUSER = "elastic" #os.getenv("ELASTICUSER")
+ELASTICPASS = "5WKt5ymymHVmJsxQ" #os.getenv("ELASTICPASS")
+
+# MariaDB
+MARIADBNAME = "my_database" #os.getenv("MARIADBNAME")
+MARIADBHOST = "localhost" #os.getenv("MARIADBHOST")
+MARIADBPORT = "32100" #os.getenv("MARIADBPORT")
+MARIADBUSER = "root" #os.getenv("MARIADBUSER")
+MARIADBPASS = "IteAgY6fBV" #os.getenv("MARIADBPASS")
+
 class SQLProcessor:
     __consumerQueue = None
     __publishQueue = None
+    __elasticClient = None
+    __mariaClient = None
+    __currentJob = None
+    __currentDoc = None
+    __currentDocId = None
+    __currentExpression = None
+    __currentSqltransform = None
 
     def __init__(self):
         self.initQueues()
+        self.connectElastic(
+            ELASTICUSER,
+            ELASTICPASS,
+            ELASTICHOST,
+            ELASTICPORT
+        )
+        self.connectMariadb(
+            MARIADBUSER,
+            MARIADBPASS,
+            MARIADBHOST,
+            MARIADBPORT
+        )
+
+    # Simple method used to connect to an elasticsearch database
+    def connectElastic(self,user,password,host,port):
+        self.__elasticClient = Elasticsearch(
+            host+":"+port,
+            basic_auth=(user,password)
+        )
+        try:
+            self.__elasticClient.info()
+            return True
+        except elastic_transport.ConnectionError:
+            # We raise an exception because the process 
+            # can't continue without a place to look for jobs
+            raise Exception("Error: Couldn't connect to Jobs database")
+
+    # Simple method used to connect to a MariaDB database
+    def connectMariadb(self,user,password,host,port):
+        try:
+            self.__mariaClient = mariadb.connect(
+                user=user,
+                password=password,
+                host=host,
+                port=int(port),
+                database=MARIADBNAME
+            )
+        except mariadb.OperationalError:
+            # We raise an exception because the process 
+            # can't continue without a place to look get
+            # information to publish in elastic
+            raise Exception("Error: Couldn't connect to MariaDB database")
+
+    def getFromElastic(self,message):
+        search = self.__elasticClient.search(index="jobs",size=1,query={"match" : {"job_id" : message["job_id"]}})
+        self.__currentJob = search["hits"]["hits"][0]["_source"]
+        search = self.__elasticClient.search(index="groups",size=1,query={"match" : {"group_id" : message["group_id"]}})
+        self.__currentDoc = search["hits"]["hits"][0]["_source"]
+        self.__currentDocId = search["hits"]["hits"][0]["_id"]
 
     def initQueues(self):
         rabbitUserPass = pika.PlainCredentials(RABBITUSER,RABBITPASS)
@@ -41,7 +112,95 @@ class SQLProcessor:
 
     def consume(self, ch, method, properties, msg):
         message = json.loads(msg)
-        print(message)
+        self.getFromElastic(message)
+        self.getExpression()
+        self.processExpression()
+        self.changeDocs()
+        self.__publishQueue.basic_publish(routing_key=RABBITPUBLISHQUEUE, body=msg, exchange='')
+        print("Group finished -> " + self.__currentDoc["group_id"])
+    
+    def getExpression(self):
+        sqltransform = None
+        transform = None
+
+        for stage in self.__currentJob["stages"]:
+            if(stage["name"] == "transform"):
+                transform = stage
+                break
+        
+        if(transform == None):
+            print(
+                "Error: No transform in stages -> " + 
+                self.__currentDoc["group_id"]
+            )
+            return True
+        
+        for transformation in transform["transformation"]:
+            if(transformation["type"] == "sql_transform"):
+                sqltransform = transformation
+                break
+
+        if(sqltransform == None):
+            print(
+                "Error: No sql_transform type of transformation in the transform stage -> " + 
+                self.__currentDoc["group_id"]
+            )
+            return True
+        
+        self.__currentExpression = sqltransform["expression"]
+        self.__currentSqltransform = sqltransform
+        return False
+        
+    def processExpression(self):
+        fieldsRegex = "(?<=%{)\\b(?!table\\b)\\b(?!doc_field\\b)[a-zA-Z0-9_]*(?=}%)"
+        fields = Regex.findall(fieldsRegex,self.__currentExpression)
+        self.__currentExpression = self.__currentExpression.replace(
+            "%{table}%",
+            self.__currentSqltransform["table"]
+            )
+
+        for field in fields:
+            if(field == "table" or field == "doc_field"):
+                continue
+            if(field not in self.__currentSqltransform["fields_mapping"]):
+                print("Error: missing field_mappings")
+                return True
+            self.__currentExpression = self.__currentExpression.replace(
+                "%{"+ field +"}%",
+                self.__currentSqltransform["fields_mapping"][field]
+            )
+
+        return False
+
+    def changeDocs(self):
+        docField = self.__currentSqltransform["doc_field"]
+        docs = self.__currentDoc["docs"]
+        fieldname = Regex.findall(
+            "(?<=SELECT )[a-zA-Z0-9_]*",
+            self.__currentExpression
+        )[0]
+        cursor = self.__mariaClient.cursor()
+        try:
+            for doc in docs:
+                cursor.execute(
+                    self.__currentExpression.replace(
+                        "%{doc_field}%",
+                        str(doc[docField])
+                    )
+                )
+                doc[fieldname] = cursor.fetchone()[0]
+        except mariadb.ProgrammingError:
+            print(
+                "Error: root.stages[name=transform].transformation[type=sql_transform].expression \
+                failed, document -> " + self.__currentDoc["group_id"]
+            )
+            return True
+        
+        self.__currentDoc["docs"] = docs
+        self.__elasticClient.index(index="groups",id=self.__currentDocId,document=self.__currentDoc)
+        return False
+
+
 
     def startProcess(self):
         self.__consumerQueue.start_consuming()
--- sqlprocessor/test/queuepublish.py
+++ sqlprocessor/test/queuepublish.py
@@ -1,5 +1,8 @@
+from tokenize import group
+from elasticsearch import Elasticsearch
 import pika
 import json
+
 # RabbitMQ
 RABBITHOST = "localhost" #os.getenv("RABBITHOST")
 RABBITPORT = "30100" #os.getenv("RABBITPORT")
@@ -8,9 +11,14 @@ RABBITPASS = "XQnpNkb23Z5RqAly" #os.getenv("RABBITPASS")
 RABBITCONSUMEQUEUE = "mysqlprocessor" #os.getenv("RABBITQUEUENAME")
 RABBITPUBLISHQUEUE = "sqlprocessor" #os.getenv("RABBITQUEUENAME")
 
+# Elastic
+ELASTICHOST = "localhost"#os.getenv("ELASTICHOST")
+ELASTICPORT = "32500"#os.getenv("ELASTICPORT")
+ELASTICPASS = "5WKt5ymymHVmJsxQ" #os.getenv("ELASTICPASS")
+
 document = {
-    "job_id" : "nuevo28392",
-    "group_id" : "nuevo28392-100"
+    "job_id" : "unid1",
+    "group_id" : "unid1-0"
 }
 
 rabbitUserPass = pika.PlainCredentials(RABBITUSER,RABBITPASS)
@@ -23,5 +31,121 @@ rabbitParameters = pika.ConnectionParameters(
 )
 
 cola = pika.BlockingConnection(rabbitParameters).channel()
-
+cola.queue_declare(queue=RABBITCONSUMEQUEUE)
 cola.basic_publish(routing_key=RABBITCONSUMEQUEUE, body=json.dumps(document), exchange= '')
+
+def elasticData():
+    elasticClient = Elasticsearch("http://"+ELASTICHOST+":"+ELASTICPORT, basic_auth=("elastic",ELASTICPASS))
+
+    if(not (elasticClient.indices.exists(index=["jobs"]))):
+        elasticClient.indices.create(index="jobs")
+
+    if(not (elasticClient.indices.exists(index=["groups"]))):
+        elasticClient.indices.create(index="groups")
+
+    listOfPersonas = [
+        ('9-8569-2630', 'Gerardo Vargas', 1, 'Heredia'),
+        ('8-7714-4896', 'Alejandro Mora', 2, 'San Jose'),
+        ('3-4540-2769', 'Esteban Rojas', 3, 'Heredia'),
+        ('8-7817-9545', 'Andres Jimenez', 4, 'Puntarenas'),
+        ('8-3370-2000', 'Mauricio Hernandez', 5, 'Limon'),
+        ('8-6225-4060', 'Ronald Castro', 6, 'Heredia'),
+        ('2-1935-3873', 'Marvin Gonzalez', 7, 'Guanacaste'),
+        ('1-7170-6152', 'Erick Ramirez', 8, 'Limon'),
+        ('5-2007-5649', 'Rafael Morales', 9, 'Limon'),
+        ('2-6593-7388', 'Juan Carlos Alvarado', 10, 'Heredia')
+    ]
+
+    job = {
+        "job_id" : "unid1",
+        "status": "new",
+        "msg": "",
+        "data_sources": [
+            {
+                "type": "elasticsearch",
+                "name": "destination_es",
+                "url": "http://databases-elasticsearch-master-hl.default.svc.cluster.local",
+                "port": "9200",
+                "usuario": "elastic",
+                "password": "Av6gG4ZBIpBYgdeZ"
+            },
+            {
+                "type" : "mysql",
+                "name": "people_db",
+                "url": "databases-mariadb-primary",
+                "port": "3306",
+                "usuario": "root",
+                "password": "LVOIjwRfuU"
+            }
+        ],
+        "control_data_source": "destination_es",
+        "source": {
+            "data_source": "people_db",
+            "expression": "SELECT * FROM persona ORDER BY cedula",
+            "grp_size": "10"
+        },
+        "stages" : [
+            {
+                "name": "extract",
+                "source_queue": "extract",
+                "destination_queue": "%{transform->transformation->add_car}%"
+            },
+            {
+                "name": "transform",
+                "transformation": [
+                    {
+                        "name": "add_car",
+                        "type": "sql_transform",
+                        "table": "car",
+                        "expression": "SELECT %{field_description}% FROM %{table}% WHERE %{field_owner}% = %{doc_field}%",
+                        "source_data_source": "database_car",
+                        "destination_data_source": "destination_es",
+                        "doc_field": "id",
+                        "source_queue": "sql_queue",
+                        "destination_queue": "%{transform->transformation->myregex}%",
+                        "fields_mapping": {
+                            "field_description": "description",
+                            "field_owner": "owner"
+                        }
+                    },
+                    {
+                        "name": "myregex",
+                        "type": "regex_transform",
+                        "regex_config": {
+                            "regex_expression": "^.* ([a-zA-z]{3}-[0-9]{3}) .*$",
+                            "group": "1",
+                            "field": "description"
+                        },
+                        "field_name": "placa",
+                        "source_queue": "regex_queue",
+                        "destination_queue": "%{load}%"
+                    }
+                ]
+            },
+            {
+                "name": "load",
+                "source_queue": "ready",
+                "destination_data_source": "destination_es",
+                "index_name": "persona"
+            }
+        ]
+    }
+
+    group = {
+        "job_id" : "unid1",
+        "group_id" : "unid1-0",
+        "docs" : []
+    }
+
+    for persona in listOfPersonas:
+        doc = {}
+        doc["cedula"] = persona[0]
+        doc["nombre"] = persona[1]
+        doc["id"] = persona[2]
+        doc["provincia"] = persona[3]
+        group["docs"].append(doc)
+    
+    elasticClient.index(index="jobs", document=job)
+    elasticClient.index(index="groups", document=group)
+
+#elasticData()
