85e2812ca1f3cd677c06d89ec04a24615b192f6c
Pamela González López <77868747+Pamlez-Tec@users.noreply.github.com>
2022-10-15 19:27:39 -0600

    Listo comentarios y métricas

--- MySQLConnector/image/app/app.py
+++ MySQLConnector/image/app/app.py
@@ -28,14 +28,20 @@ MARIADBPORT = os.getenv("MARIADBPORT")
 MARIADBUSER = os.getenv("MARIADBUSER")
 MARIADBPASS = os.getenv("MARIADBPASS")
 
+#----------------------------------------------------------------------- 
 # Class for printing colors
+#----------------------------------------------------------------------- 
 class bcolors:
     OK      = '\033[92m'    #GREEN
     WARNING = '\033[93m'    #YELLOW
     FAIL    = '\033[91m'    #RED
     RESET   = '\033[0m'     #RESET COLOR
-
+    
+#----------------------------------------------------------------------- 
+#Main class
+#----------------------------------------------------------------------- 
 class MySQLConnector:
+    
     ElasticClient = None
     MariaClient = None
 
@@ -50,8 +56,10 @@ class MySQLConnector:
         self.startMetrics()
         self.connectDbElastic(ELASTICHOST,ELASTICPORT,ELASTICUSER,ELASTICPASS)
         self.startConsume(RABBITUSER,RABBITPASS,RABBITHOST,RABBITPORT,RABBITQUEUENAME)
-
-
+        
+    #----------------------------------------------------------------------- 
+    # Function that creates an elastic client
+    #----------------------------------------------------------------------- 
     def connectDbElastic(self,pHost,pPort,pUser,pPass):
         try:
             self.ElasticClient = Elasticsearch(
@@ -60,7 +68,10 @@ class MySQLConnector:
                 )
         except elastic_transport.ConnectionError:
             raise Exception ("Error: Couldn't connect to database Elastic")
-    
+            
+    #-----------------------------------------------------------------------         
+    # Function that creates an maria client
+    #----------------------------------------------------------------------- 
     def connectDbMaria(self,pHost,pPort,pUser,pPass,pName):
         try:
             self.MariaClient = mariadb.connect(
@@ -71,9 +82,11 @@ class MySQLConnector:
                     database= pName)
         except:
             print("Error: Couldn't connect to database Maria") 
-    
-    def startMetrics(self):
-        
+            
+    #-----------------------------------------------------------------------        
+    # Setting metrics and start server for metrics
+    #----------------------------------------------------------------------- 
+    def startMetrics(self):    
         self.TOTAL_TIME = Gauge(
             'total_processing_time', 
             'Total amount of time elapsed when processing'
@@ -94,7 +107,10 @@ class MySQLConnector:
         self.NUMBER_PROCESSEDGROUPS.set(0)
 
         start_http_server(6945)
-
+        
+    #-----------------------------------------------------------------------    
+    # Function that opens channel and consumes message sent by orchestrator
+    #-----------------------------------------------------------------------
     def startConsume(self,pUser,pPass,pHost,pPort,pQueue):
         credentials_ = pika.PlainCredentials(pUser, pPass)
         parameters = pika.ConnectionParameters(host=pHost, port=pPort, credentials=credentials_)
@@ -104,7 +120,10 @@ class MySQLConnector:
         channelConsuming.basic_consume(queue=pQueue, on_message_callback= self.callback, auto_ack=False)
 
         channelConsuming.start_consuming()
-
+        
+    #----------------------------------------------------------------------- 
+    # Function that opens channel and sends message to the second queue
+    #----------------------------------------------------------------------- 
     def startProduce(self,pUser,pPass,pHost,pPort,pQueue,pMsg):
         print(f"{bcolors.OK} MySQL Connector: {bcolors.RESET} Send message")
         credentials2 = pika.PlainCredentials(pUser, pPass)
@@ -115,7 +134,11 @@ class MySQLConnector:
         msg2 =pMsg
         channel2.basic_publish(exchange='', routing_key=RABBITQUEUEMYSQL, body=msg2) 
         connection2.close()
-
+        
+    #----------------------------------------------------------------------- 
+    # Function that converts the orchestrator message to a json object, 
+    # works with that object and updates metrics
+    #----------------------------------------------------------------------- 
     def callback (self, ch, method, properties, body):
         sleep(1) #
         json_msg = json.loads(body)
@@ -134,36 +157,46 @@ class MySQLConnector:
         ch.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
         print(f"{bcolors.OK} MySQL Connector: {bcolors.RESET} Process finished")
 
-
+    #----------------------------------------------------------------------- 
+    # Main function of the application where it works with the message.
+    #----------------------------------------------------------------------- 
     def work(self, pMsg, pBody):
         print(f"{bcolors.OK} MySQL Connector: {bcolors.RESET} Process started")
-
-        json_job = self.ElasticClient.search(index='jobs',size=1,query={"match":{"job_id":pMsg["job_id"]}}) 
+        #We look in the jobs index for the recent json document and we get the document
+        json_job = self.ElasticClient.search(index='jobs',size=1,query={"match":{"job_id":pMsg["job_id"]}})
         idDocumento = json_job["hits"]["hits"][0]["_id"] 
         loadJson = self.ElasticClient.get(index="jobs", id=idDocumento)["_source"]
         groupId=pMsg["groud_id"] 
-        jobid = pMsg["job_id"]    
-        #_____________________Datos para hacer consulta_______________________
-        expresion = loadJson["source"]["expression"] #El select que debe ejecutar en la base de datos    
-        groupSize = loadJson["source"]["grp_size"]   
-        startRow = groupId[self.counterWord(groupId):]  
-        query = expresion+" LIMIT "+startRow+","+groupSize
-        #____________________Consultar y transformar__________________________  
+        jobid = pMsg["job_id"]   
+        
+        #---------> Data to make the query, we get it from the loaded document
+        expresion = loadJson["source"]["expression"] #The select you should execute on the database   
+        groupSize = loadJson["source"]["grp_size"] 
+        startRow = groupId[self.counterWord(groupId):] 
+        query = expresion+" LIMIT "+startRow+","+groupSize #Final query
+        
+        #---------> In this part, the final query is made to MariaDB and once the result is transformed into a list
         json_output = json.dumps(self.transformacion (query, (groupSize,)))
         documentUpdate = self.ElasticClient.search(index='groups',size=1,query={"match":{"groud_id":groupId}})
-        #____________________Inicio de la actualizacion___________________________
+        
+        #---------> Update to elastic
         self.update(documentUpdate,jobid,groupId,json_output)
-        #___________________Publicamos en la segunda cola________________________
+        
+        #---------> When all the work is done, we publish to the second queue
         self.startProduce(RABBITUSER,RABBITPASS,RABBITHOST,RABBITPORT,RABBITQUEUEMYSQL,pBody)
 
     def update(self, pDoc, pJobId, pGrpId, pJson):
         try:
             idHit = pDoc["hits"]["hits"][0]["_id"]
-            data = {"job_id": pJobId, "group_id": pGrpId, 'docs':pJson} #Se annade del fiel docs
-            self.ElasticClient.index(index='groups', id=idHit,body=data) #Sobreescribo documento
+            data = {"job_id": pJobId, "group_id": pGrpId, 'docs':pJson} #Added fiel docs
+            self.ElasticClient.index(index='groups', id=idHit,body=data) #Overwrite document
         except:
             print("Error. Can't update")
-
+            
+    #----------------------------------------------------------------------- 
+    # This function connects to MariaDb, makes the query and transforms that query to a list of type key : value
+    # Return that transformation
+    #----------------------------------------------------------------------- 
     def transformacion (self, query, args=(), one=False):
         self.connectDbMaria(MARIADBHOST,MARIADBPORT,MARIADBUSER,MARIADBPASS,MARIADBNAME) 
         try:
@@ -175,7 +208,12 @@ class MySQLConnector:
                 for i, value in enumerate(row)) for row in cur.fetchall()]
         cur.connection.close()
         return (queryResult[0] if queryResult else None) if one else queryResult
-
+    
+    #----------------------------------------------------------------------- 
+    # Count the letters to - 
+    # For example: Orchestrator sends a message with a key group_id and its value 
+    # is of type jobid-number. We need to count the letters up to the hyphen in order to get the number
+    #----------------------------------------------------------------------- 
     def counterWord(self, pWord):
         control = 0
         for i in pWord:
